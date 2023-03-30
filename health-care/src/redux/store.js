3e9420fbd0db1d5e69b67ca9cb1f82e69d8db77d
Manoj Kumar <officailmanoj14@gmail.com>
2023-03-30 22:56:17 +0530

    added admin dashboard

--- health-care/src/redux/store.js
+++ health-care/src/redux/store.js
@@ -2,8 +2,9 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import { reducer as authReducer } from "./authReducer/reducer";
 import { reducer as productReducer } from "./productReducer/reducer";
+import { reducer as adminReducer } from "./admin/admin.reducer";
 
-const rootReducer=combineReducers({authReducer,productReducer})
+const rootReducer=combineReducers({authReducer,productReducer, adminReducer});
 
 
 export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))
