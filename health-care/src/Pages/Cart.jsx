4d3d4aa536c25bcf18e7505e51af90a18ab15da0
Rahulaustralia <rahul090897pandey@gmail.com>
2023-03-31 03:54:55 +0530

    Cart Page UI

--- health-care/src/Pages/Cart.jsx
+++ health-care/src/Pages/Cart.jsx
@@ -1,47 +1,129 @@
-import { Box, Radio, Text, Image, Select, Button, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Radio,
+  Text,
+  Image,
+  Select,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
 
 import { ChevronDownIcon, AddIcon } from "@chakra-ui/icons";
 
-
 import axios from "axios";
-import CartCarousel from "./CartCarousel";
+import { changeQuantityAction, deleteAction, getDataAction } from "../redux/CartReducer/action";
+// import CartCarousel from "./CartCarousel";
 
 const Cart = () => {
-
-
+  let dispatch = useDispatch();
+  let { cart,total } = useSelector((state) => state.cartReducer);
+  useEffect(() => {
+    dispatch(getDataAction);
+  }, []);
+  let handleDelete = (id) => {
+    dispatch(deleteAction(id));
+  };
+  let handleSelect = (value,id) => {
+    dispatch(changeQuantityAction(value,id))
+  };
   return (
     <>
-    
       <Box height={"auto"} bg={"#F3F7FB"}>
-        <Box flexWrap="wrap" p={5} width={"80%"} m={"auto"} height={"auto"} display={{base:"none",md:"grid"}}>
+        <Box
+          flexWrap="wrap"
+          p={5}
+          width={"80%"}
+          m={"auto"}
+          height={"auto"}
+          display={{ base: "none", md: "grid" }}
+        >
           <Text>Order Summary</Text>
 
-          <Box display={"flex"}>
-            <Box ml={-10} >
-              <CartCarousel/>
-            </Box>
-            
-          </Box>
         </Box>
 
         <Box
           display={"grid"}
-          gridTemplateColumns={{base:"repeat (2,1fr)",md:"repeat(1,1fr)"}}
+          gridTemplateColumns={{ base: "repeat (2,1fr)", md: "repeat(1,1fr)" }}
           gap={8}
           mt={5}
-         
-         
           height={"auto"}
         >
-          <Box bg={"white"} p={0} rounded={5} height={"auto"}  >
+          <Box bg={"white"} p={0} rounded={5} height={"auto"}>
             <Text p={5} pb={0} fontSize={"lg"} color={"grey"} mb={0}>
               PRODUCTS
             </Text>
-            
-        
+            {cart.map((el) => (
+              <Box
+                key={el.id}
+                fontSize={"md"}
+                display={"flex"}
+                height={"auto"}
+                lineHeight={6}
+                borderBottom={"1px solid grey"}
+                p={3}
+              >
+                <Box>
+                  <Image height={"60%"} ml={40} mr={20} mt={5} src={el.img} alt="" />
+                </Box>
+                <Box>
+                  <Text fontSize={"md"} fontWeight={500}>
+                    {el.title}
+                  </Text>
+                  <Text fontSize={"small"} color={"green"}>
+                    Only 6 Left In Stock
+                  </Text>
+                  <Text fontSize={"small"} as={"i"}>
+                    Mfr:{el.brand}
+                  </Text>
+                  <Box display={"flex"} gap={80}>
+                    <Text
+                      fontSize={"md"}
+                      color={"#ff6f61"}
+                      fontWeight={500}
+                      pt={2}
+                    >
+                      RS.{Number(el.price) * el.quantity}.00
+                    </Text>
+                    <Select
+                      height={8}
+                      mb={6}
+                      width={40}
+                      onChange={(e) => handleSelect(e.target.value, el.id)}
+                    >
+                    <option>quantity: {el.quantity}</option>
+                      <option value="1">quantity: 1</option>
+                      <option value="2">quantity: 2</option>
+                      <option value="3">quantity: 3</option>
+                      <option value="4">quantity: 4</option>
+                      <option value="5">quantity: 5</option>
+                      <option value="6">quantity: 6</option>
+                    </Select>
+                  </Box>
+                  <Box display={"flex"}>
+                    <Box pr={2} borderRight={"1px solid grey"}>
+                      <Text> Delivery between Nov 11 6PM-Nov 18 10PM </Text>
+                    </Box>
+                    <Box>
+                      <Button
+                        onClick={() => handleDelete(el.id)}
+                        bg={"#F6F6F7"}
+                        mr={0}
+                        ml={3}
+                        mt={3}
+                        size={"sm"}
+                        color={"red"}
+                      >
+                        REMOVE
+                      </Button>
+                      
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+            ))}
 
             <Box
               color={"teal"}
@@ -59,15 +141,7 @@ const Cart = () => {
             </Box>
           </Box>
 
-          <Box
-            bg={"white"}
-            rounded={"md"}
-            p={5}
-            pr={8}
-           
-            height={300}
-           
-          >
+          <Box bg={"white"} rounded={"md"} p={5} pr={8} height={300}>
             <Text mb={1} color={"grey"} fontSize={"lg"}>
               PAYMENT DETAILS
             </Text>
@@ -78,17 +152,9 @@ const Cart = () => {
               display={"flex"}
             >
               <Text> MRP Total</Text>
-              <Text>Rs,{"bag"}</Text>
+              <Text>Rs,{total}</Text>
             </Box>
-            <Box
-              fontSize={"sm"}
-              mt={3}
-              justifyContent={"space-between"}
-              display={"flex"}
-            >
-              <Text> Medicine Hub Discount</Text>
-              <Text>-Rs,75.00</Text>
-            </Box>
+           
             <Box
               fontSize={"md"}
               fontWeight={500}
@@ -97,20 +163,6 @@ const Cart = () => {
               display={"flex"}
             >
               <Text>Total Amount*</Text>
-              <Text>Rs,{"discountedPrice"}</Text>
-            </Box>
-            <Box
-              bg={"#F3F8EC"}
-              p={2}
-              fontWeight={500}
-              fontSize={"smaller"}
-              mt={3}
-              justifyContent={"space-between"}
-              display={"flex"}
-            >
-              <Text pl={4} color={"green"}>
-                TOTAL SAVINGS Rs.75
-              </Text>
             </Box>
 
             <Box
@@ -122,12 +174,11 @@ const Cart = () => {
             >
               <Box>
                 <Text fontSize={"md"}>TOTAL AMOUNT</Text>
-                <Text fontSize={"larger"}>Rs,{"discountedPrice"}</Text>
               </Box>
               <Box>
                 <Link to="/checkout/payment-information">
                   {" "}
-                  <Button size={"lg"}>PROCEED</Button>{" "}
+                  <Button style={{backgroundColor:"#ff6f61", color:"white",}} size={"lg"}>CHECKOUT</Button>{" "}
                 </Link>
               </Box>
             </Box>
