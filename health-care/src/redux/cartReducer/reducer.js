1d50b6c60eaa76ca038f7a232459bf68a75e2293
Rahulaustralia <rahul090897pandey@gmail.com>
2023-03-31 04:05:51 +0530

    Cart Page Functionality Part

---
+++ health-care/src/redux/cartReducer/reducer.js
@@ -0,0 +1,23 @@
+let initial = {
+  cart: JSON.parse(localStorage.getItem("cart")) || [],
+  total: 0,
+};
+
+export let reducer = (state = initial, { type, payload }) => {
+  switch (type) {
+    case "DELETE_CART": {
+      return { ...state, cart: payload };
+    }
+    case "GET_DATA": {
+      return { ...state, cart: payload };
+    }
+    case "CHANGE_QUANTITY": {
+      return { ...state, cart: payload };
+    }
+    case "CALCULATE_PRICE": {
+      return { ...state, total: payload };
+    }
+    default:
+      return state;
+  }
+};
