ad166826a763a256b02a318abe73d07d54a731b5
tambeaditya101 <tambeaditya101@gmail.com>
2023-03-30 13:35:25 +0530

    fw21_0332_day2-task-completed

--- health-care/src/Pages/ProductItem.jsx
+++ health-care/src/Pages/ProductItem.jsx
@@ -1,11 +1,10 @@
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
+import { StarIcon } from "@chakra-ui/icons";
 
 function ProductItem({ ...el }) {
   return (
     <Box
-      //border={"1px solid grey"}
-      //borderRadius="lg"
       p={4}
       boxShadow={
         " rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px"
@@ -13,7 +12,7 @@ function ProductItem({ ...el }) {
     >
       <img style={{ margin: "auto" }} src={el.img} alt="product_image" />
       <Text fontSize="lg">
-        Title: <b>{el.title}</b>
+        Title: <b>{el.title} </b>
       </Text>
       <Text fontSize="md">
         Brand: <b>{el.brand}</b>
@@ -22,7 +21,10 @@ function ProductItem({ ...el }) {
         Price: <b>₹{el.price}</b>
       </Text>
       <Text fontSize="sm">
-        Rating: <b>{el.rating}</b>
+        Rating:
+        <b>
+          {el.rating} <StarIcon />
+        </b>
       </Text>
     </Box>
   );
