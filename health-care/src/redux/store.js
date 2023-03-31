76936871da707cce55d8c1ef87c3a041bc6015c9
Rahulaustralia <rahul090897pandey@gmail.com>
2023-04-01 02:26:18 +0530

    Checkout Page UI

--- health-care/src/redux/store.js
+++ health-care/src/redux/store.js
@@ -3,9 +3,13 @@ import thunk from "redux-thunk";
 import { reducer as authReducer } from "./authReducer/reducer";
 import { reducer as productReducer } from "./productReducer/reducer";
 import { reducer as adminReducer } from "./adminReducer/reducer";
+import { reducer as cartReducer } from "./cartReducer/reducer";
 
-const rootReducer=combineReducers({authReducer,productReducer,adminReducer})
+const rootReducer = combineReducers({
+  authReducer,
+  productReducer,
+  adminReducer,
+  cartReducer,
+});
 
-
-
-export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
