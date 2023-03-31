d7e68b579e270e0edd006eb8477a98b76a2c3db3
prachi913 <prachitiwari0911@gmail.com>
2023-04-01 01:02:25 +0530

    day5

--- health-care/src/App.jsx
+++ health-care/src/App.jsx
@@ -4,13 +4,15 @@ import "./App.css";
 import WithSubnavigation from "./landingcomponent/PagesMain/Navbar";
 // import ProductList from "./Pages/ProductList";
 import { Mainroute } from "./landingcomponent/PagesMain/Mainroute";
+import { Footer2 } from "./landingcomponent/Footer2";
 // import ProductList from "./Pages/ProductList";
 
 function App() {
   return (
     <div className="App">
       <WithSubnavigation />
-     <Mainroute/>
+      <Mainroute />
+      <Footer2/>
     </div>
   );
 }
